cba53d9a3af42eb2f9e9a0c6a67762c89d30c79f
Lindsay Wall <lindsay.is@gmail.com>
2017-10-31 18:51:04 -0400

    logs responses to log.txt

--- liri.js
+++ liri.js
@@ -14,22 +14,9 @@ if (command === "my-tweets") {
 } else if (command === "movie-this") {
 	movieThis();
 } else if (command === "do-what-it-says") {
-	fs.readFile("random.txt", "utf8", function(error, data) {
-		if (error) {
-			return console.log(error);
-		} else {
-			var dataArr = data.split(",");
-			userInput = dataArr[1];
-
-			if (dataArr[0] === "my-tweets") {
-				tweetTweet();
-			} else if (dataArr[0] === "spotify-this-song") {
-				spotifyThis();
-			} else {
-				movieThis();
-			}
-		}
-	});
+	fileSaysDo();
+} else {
+	console.log("I'm sorry, I don't understand. Please tell me a command: \nmy-tweets \nspotify-this-song \nmovie-this \ndo-what-it-says");
 }
 
 function tweetTweet() {
@@ -41,6 +28,12 @@ function tweetTweet() {
 			for (var i = 0; i < tweet.length; i++) {
 				console.log(tweet[i].created_at);
 				console.log(tweet[i].text);
+
+				fs.appendFile("log.txt", "\n" + tweet[i].created_at + "\n" + tweet[i].text, function(err) {
+					if (err) {
+						console.log(err);
+					}
+				})
 			}
 		}
 	})
@@ -63,6 +56,16 @@ function spotifyThis() {
 			console.log("External url: " + data.tracks.items[0].album.external_urls.spotify) // external link
 			console.log("Album: " + data.tracks.items[0].album.name) // album name
 		}
+
+		fs.appendFile("log.txt", "\nAppending this song and artist data: " + 
+			"\n" + data.tracks.items[0].album.artists[0].name + 
+			"\n" + data.tracks.items[0].name + 
+			"\n" + data.tracks.items[0].album.external_urls.spotify + 
+			"\n" + data.tracks.items[0].album.name, function(err) {
+				if (err) {
+					console.log(err);
+				}
+			})
 	})
 }
 
@@ -84,5 +87,41 @@ function movieThis() {
 			console.log("Plot: " + JSON.parse(body).Plot);
 			console.log("Actors: " + JSON.parse(body).Actors);	
 		}
+
+		fs.appendFile("log.txt", "\n" + "Appending this movie information: " + 
+			"\n" + JSON.parse(body).Title + "\n" + JSON.parse(body).Year + 
+			"\n" + JSON.parse(body).imdbRating + "\n" + JSON.parse(body).rottenExists +
+			"\n" + JSON.parse(body).Country + "\n" + JSON.parse(body).Language +
+			"\n" + JSON.parse(body).Plot + "\n" + JSON.parse(body).Actors, function(err) {
+				if (err) {
+					console.log(err);
+				}
+			})
 	})
+}
+
+function fileSaysDo() {
+	fs.readFile("random.txt", "utf8", function(error, data) {
+		if (error) {
+			return console.log(error);
+		} else {
+			var dataArr = data.split(",");
+			userInput = dataArr[1];
+			command = dataArr[0];
+
+			if (command === "my-tweets") {
+				tweetTweet();
+			} else if (command === "spotify-this-song") {
+				spotifyThis();
+			} else {
+				movieThis();
+			}
+		}
+
+		fs.appendFile("log.txt", "User engaged the random file.", function(err) {
+			if (err) {
+				console.log(err);
+			}
+		})
+	});
 }
